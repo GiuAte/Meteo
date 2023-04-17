baccd881ebd78091ae09f006f71ba2ab4fbe269b
GiuAte <giulioaterno@gmail.com>
2023-04-17 14:23:00 +0200

    Initial Commit

---
+++ MeteoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MeteoApp
+//
+//  Created by Giulio Aterno on 17/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
